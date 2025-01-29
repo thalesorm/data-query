bbc799f917ecc3bc0955011dcb3aba4773b9417a
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2025-01-29 18:48:14 -0300

    :sparkles: feat: Allow user to enter a specific user ID

--- src/main.py
+++ src/main.py
@@ -6,14 +6,21 @@ def display_users(users):
         for user in users:
             print(f"ID: {user['id']} - Nome: {user['name']}")
 
+
 def main():
-    """Função principal pra gerenciar a interação com o usuário."""
+    """Função principal que gerencia a interação com o usuário."""
     users = get_users()
     if not users:
         print("Não foi possível obter os usuários.")
         return
 
     display_users(users)
+
+    try:
+        int(input("\nDigite o ID do usuário para obter detalhes: "))
+    except ValueError:
+        print("ID inválido! Por favor, insira um número.")
+        return
 
 if __name__ == "__main__":
     main()

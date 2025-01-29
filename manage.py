f4a6a4d5afabc1403d241f9d391605740e2e3820
Thales Santos <85639311+thalesorm@users.noreply.github.com>
2025-01-29 20:17:32 -0300

    :zap: perf: command manager

---
+++ manage.py
@@ -0,0 +1,20 @@
+import subprocess
+import sys
+import os
+
+def run(command):
+    subprocess.run(command, shell=True)
+
+if __name__ == "__main__":
+    
+    python_path = os.path.abspath("src")
+    os.environ["PYTHONPATH"] = python_path  # Define o PYTHONPATH
+
+    if sys.argv[1] == 'install':
+        run('python -m venv venv && .\\venv\\Scripts\\activate && pip install -r requirements.txt')
+    elif sys.argv[1] == 'start':
+        run('.\\venv\\Scripts\\activate && python src\\main.py')
+    elif sys.argv[1] == 'test':
+        run('.\\venv\\Scripts\\activate && pytest tests')
+    else:
+        print('Comando inválido. Use install, start ou test.')
